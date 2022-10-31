ec7fb345758c016095898d98cfe8b944c8cf7d6b
ralgond <ht201509@163.com>
2022-10-31 22:16:23 +0800

    fix bug

--- Augment/main.py
+++ Augment/main.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 
 from common import set_seed, TrainDataset, TestDataset, minmax_scale, create_window_list, augament
-from network import UNet4, SimpleCnnNet
+from network import SimpleCnnNet
 
 
 class Channel:
